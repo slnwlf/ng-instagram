ec4b763d3dfd054ef66e72d92b7bb55b6af5766a
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2016-01-22 09:58:12 -0800

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-var app = angular.module('instagramSearchApp', []);
+var app = angular.module('instagramSearchApp', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: '/templates/search.html',
+			controller: 'SearchCtrl'
+		})
+		.when('/favorites', {
+			templateUrl: '/templates/favorites.html',
+			controller: 'FavoritesCtrl'
+		});
+
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	});
+}]);
+
+app.controller('SearchCtrl', ['$scope', function ($scope) {
+	// add a test attribute here
+	$scope.searchCtrlTest = "search test string here";
+}]);
+
+app.controller('FavoritesCtrl', ['$scope', function ($scope) {
+	// add a test attribute
+	$scope.favoritesCtrlTest = "favorites test string here";
+}])
